545f11a5262432c57d42987c2ae3ec3c2a95fedf
Benjamin Porchet <benjaminporchet.pro@gmail.com>
2022-07-22 15:21:27 +0200

    Birthday factory data

---
+++ database/factories/BirthdayFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Birthday>
+ */
+class BirthdayFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->firstName,
+            'birth_date' => $this->faker->date,
+            'phone' => $this->faker->phoneNumber
+        ];
+    }
+}
